8de5bdd370d290787ba5f80a418b7b920cf3a77f
Jeff <jeff.newburn@theironyard.com>
2015-10-28 12:27:45 -0700

    Adding more views and forms

--- chirper/chirp/urls.py
+++ chirper/chirp/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from chirp.views import ListChirps
 
+# Note that the as_view() has to be called to convert a CBV to a normal view
 urlpatterns = [
-    url(r'^$', 'chirp.views.list_chirps', name='list_chirps'),
-    url(r'^(?P<chirp_id>\d+)/$', 'chirp.views.chirp_detail', name='chirp_detail')
+    url(r'^$', ListChirps.as_view(), name='list_chirps'),
+    url(r'^(?P<chirp_id>\d+)/$', 'chirp.views.chirp_detail',
+        name='chirp_detail'),
+    url(r'^create/$', 'chirp.views.chirp_create', name='chirp_create')
 ]
