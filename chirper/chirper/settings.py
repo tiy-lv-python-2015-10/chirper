eea83770eb1b99b48bcaf55ed71379eea9145a51
Jeff <jeff.newburn@theironyard.com>
2015-11-04 12:35:12 -0800

    Adding static and bootstrap

--- chirper/chirper/settings.py
+++ chirper/chirper/settings.py
@@ -105,7 +105,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "global"),
+)
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = reverse_lazy('list_chirps')
