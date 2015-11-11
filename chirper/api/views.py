8653a360f2e7f7069c3755e2a910f2c9da0e98a1
Jeff <jeff.newburn@theironyard.com>
2015-11-11 11:10:01 -0800

    Changing to generic views.  Adding pagination and filters

--- chirper/api/views.py
+++ chirper/api/views.py
@@ -7,63 +7,43 @@ from django.utils import timezone
 from django.views.generic import View
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from api.serializers import ChirpSerializer, UserSerializer
 from chirp.models import Chirp
+from rest_framework import generics
 
-@api_view(['GET'])
-def list_users(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True, context={'request': request})
-    return Response(serializer.data)
+class SmallPagination(PageNumberPagination):
+    page_size = 10
 
 
-@api_view(['GET', 'POST'])
-def list_create_chirps(request):
-
-    if request.method == "GET":
-        chirps = Chirp.objects.all()
-        serializer = ChirpSerializer(chirps, many=True)
-
-        return Response(serializer.data)
-
-    elif request.method == "POST":
-        user = User.objects.get(pk=1)
-
-        serializer = ChirpSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ListUsers(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
+class ListCreateChirp(generics.ListCreateAPIView):
+    queryset = Chirp.objects.order_by('-posted_at')
+    serializer_class = ChirpSerializer
+    # The pagination class will override the settings in the REST_FRAMEWORK
+    pagination_class = SmallPagination
 
-class DetailUpdateChirp(APIView):
+    def perform_create(self, serializer):
+        serializer.save(author=User.objects.get(pk=1))
 
-    def get(self, request, pk, format=None):
-        """
-        Details about the chirp
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        chirp = get_object_or_404(Chirp, pk=pk)
-        serializer = ChirpSerializer(chirp)
-        return Response(serializer.data)
+    def get_queryset(self):
+        qs = super().get_queryset()
+        username = self.request.query_params.get('username', None)
+        if username:
+            qs = qs.filter(author__username=username)
 
-    def put(self, request, pk, format=None):
-        chirp = get_object_or_404(Chirp, pk=pk)
-        serializer = ChirpSerializer(chirp, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+        keyword = self.request.query_params.get('keyword', None)
+        if keyword:
+            qs = qs.filter(title__icontains=keyword)
+        return qs
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        chirp = get_object_or_404(Chirp, pk=pk)
-        chirp.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class DetailUpdateChirp(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chirp.objects.all()
+    serializer_class = ChirpSerializer
